37bd1d3f22735b9e8b4718b7251fc21f8beb4755
ryu-k67 <o2ro8chi@gmail.com>
2022-10-20 23:18:47 +0900

    gitignoreの設定

--- block_breaker.py
+++ block_breaker.py
@@ -2,6 +2,10 @@ import pygame
 
 def main():
     pygame.init()
+    pygame.display.set_caption('ブロック崩し')
+    screen = pygame.display.set_mode((400,400))
+    screen.fill(0,0,0)
+
 
 if __name__=="__main__":
     main()
