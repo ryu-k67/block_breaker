0fab20bd24945a316d0f0ee2f24a0664a66938e3
ryu-k67 <o2ro8chi@gmail.com>
2022-10-22 00:10:16 +0900

    blockの配置と開始準備

--- block_breaker.py
+++ block_breaker.py
@@ -7,13 +7,13 @@ import sys
 SCREEN=Rect(0,0,400,600)
 
 class Block(pygame.sprite.Sprite):
-    def __init__(self,file):
+    def __init__(self,file,x,y):
         pygame.sprite.Sprite.__init__(self)
         self.image=pygame.image.load(file).convert_alpha()
-        self.image=pygame.transform.scale(self.image,(100,50))
+        self.image=pygame.transform.scale(self.image,(SCREEN.width/8,SCREEN.height*0.4/4))
         self.rect=self.image.get_rect()
-        self.rect.left=0
-        self.rect.top=0
+        self.rect.left=x*self.rect.width
+        self.rect.top=y*self.rect.height
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
@@ -21,16 +21,26 @@ class Block(pygame.sprite.Sprite):
 
 
 class Ball(pygame.sprite.Sprite):
-    def __init__(self,file):
+    def __init__(self,file,paddle):
         pygame.sprite.Sprite.__init__(self)
         self.image=pygame.image.load(file).convert_alpha()
         self.image=pygame.transform.scale(self.image,(25,25))
         self.rect=self.image.get_rect()
-        self.rect.left=0
-        self.rect.top=300
+        self.paddle=paddle
+        self.update=self.start
+        self.rect.bottom=paddle.rect.top
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+
+    def start(self):
+        self.rect.centerx=self.paddle.rect.centerx
+        
+    def change(self):
+        self.update=self.move
+
+    def move(self):
+        self.rect.centery=SCREEN.top
 
 
 
@@ -41,6 +51,7 @@ class Paddle(pygame.sprite.Sprite):
         self.image=pygame.transform.scale(self.image,(100,20))
         self.rect=self.image.get_rect()
         self.rect.bottom=SCREEN.bottom
+        self.rect.centerx=int(SCREEN.width/2)
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
@@ -58,15 +69,19 @@ def main():
     screen = pygame.display.set_mode(SCREEN.size)
     screen.fill((0,0,0))
 
-    block=Block('block.png')
-    ball=Ball('ball.png')
+    blocks=pygame.sprite.Group()
+    for ball_x in range(8):
+        for ball_y in range(4):
+            block=Block('block.png',ball_x,ball_y)
+            blocks.add(block)
     paddle=Paddle('paddle.png')
-
+    ball=Ball('ball.png',paddle)
     while True:
         screen.fill((0,0,0))
         paddle.move()
+        ball.update()
 
-        block.draw(screen)
+        blocks.draw(screen)
         ball.draw(screen)
         paddle.draw(screen)
         pygame.display.update()
@@ -75,7 +90,10 @@ def main():
             if event.type==QUIT:
                 exit()
             elif event.type==KEYDOWN:
-                exit()
+                if event.key==K_RETURN:
+                    ball.change()
+                else:
+                    exit()
 
 
 
